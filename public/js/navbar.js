a26c31ce60222ce310b25ae153805d0efea4dbde
Egor Sharapkov <71561175+EgorGrib@users.noreply.github.com>
2022-03-21 16:16:06 +0300

    lab2-upd

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
-    let reference = document.location.href.split('/')[4].split('.')[0];
+    let reference = document.location.href.split('/')[3].split('.')[0];
     console.log(reference);
     switch(reference){
         case '':
@@ -7,16 +7,16 @@ window.addEventListener("load", () => {
             document.getElementsByClassName('menu-item')[0].classList.add('active-item');
             break;
         case 'iphone':
-            document.getElementsByClassName('menu-item')[1].classList.add('active-item');
+            document.getElementsByClassName('nav-item')[0].classList.add('active-item');
             break;
         case 'macbook':
-            document.getElementsByClassName('menu-item')[2].classList.add('active-item');
+            document.getElementsByClassName('nav-item')[1].classList.add('active-item');
             break;
         case 'ipad':
-            document.getElementsByClassName('menu-item')[3].classList.add('active-item');
+            document.getElementsByClassName('nav-item')[2].classList.add('active-item');
             break;
         case 'watch':
-            document.getElementsByClassName('menu-item')[4].classList.add('active-item');
+            document.getElementsByClassName('nav-item')[3].classList.add('active-item');
             break;
     }
 });
